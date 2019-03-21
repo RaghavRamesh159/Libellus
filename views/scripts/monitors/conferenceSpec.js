e53ee8dc1c183e208a09ca9da1b788b1196e4a6b
Raghav Ramesh <raghavendranr.1504@gmail.com>
2019-03-21 22:08:23 +0530

    validate

--- views/scripts/monitors/conferenceSpec.js
+++ views/scripts/monitors/conferenceSpec.js
@@ -10,5 +10,76 @@ let constraints = {
 			pattern: "[A-Z][a-z]+",
 			message: "Must start with capital and can only contain alphabets"
 		}
+	},	
+	researchArea: {
+		presence: true,
+		length: {
+			minimum: 2,
+			maximum: 20
+		},
+		format: {
+			pattern: "[A-Za-z0-9_-: ]",
+			message: "can contain only letters, numbers and _-:"
+			}
+		},
+	paperTitle: {
+		presence: true,
+		format: {
+			pattern: "[A-Z][a-zA-Z-_:]*",
+			message: "must start with capital and can contain only letters, underscore and hyphen"
+			}
+		},
+	doi: {
+		presence:true,
+		format: {
+			pattern: "\d{8}",
+			message: "must be 8 digits"
+		}
 	},
+	isbn: {
+		presence: true,
+		format: {
+			pattern: "\d{4}\-\d{4}",
+			message: "must be of the format dddd-dddd"
+		}
+	},
+	
+	confernceName: {
+		presence: true,
+		format: {
+			pattern: "[A-Z][]"
+		}
+	},
+	
+	fromDate: {
+		presence: true,
+		date: {
+			latest: moment().subtract(1, "days"),
+			message: "^Cant submit future publications"
+		}
+	},
+		
+	toDate: {
+		presence: true,
+		date: {
+			latest: moment().subtract(1, "days"),
+			message: "^Cant submit future publications"
+		}
+	},
+	pages: {
+		presence: true,
+		numericality: {
+			onlyInteger: true,
+			greaterThanOrEqualTo: 1
+		}
+	},
+	
+	abstract: {
+		presence: true
+	},
+	
+	keywords: {
+		presence: true
+		//Comma seperated regEx havent done yet
+	},	
 };
